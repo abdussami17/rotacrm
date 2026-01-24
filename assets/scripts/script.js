760193e39292cf2b849a798cca8e054493a1a006
abdussami17 <s.stechpoint123@gmail.com>
2026-01-24 21:57:47 +0500

    modal chnages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -468,3 +468,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = [].slice.call(
+        document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+
+    tooltipTriggerList.forEach(function (el) {
+        new bootstrap.Tooltip(el, {
+            trigger: 'hover focus',
+            container: 'body'
+        });
+    });
+});
+
